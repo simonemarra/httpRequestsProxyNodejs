e72785f7592f425f97ebbaa463714dc072fcac46
Simone Marra <simone.marra.electronics@gmail.com>
2020-12-09 14:17:30 +0100

    changed request with node-fetch, reduced response headers

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 var http = require('http');
-const request = require('request');
+// const request = require('request');
+const fetch = require('node-fetch');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { resolveSoa } = require('dns');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// Remove the X-Powered-By headers.
+const removeThoseHeaders = [
+    'x-powered-by',
+    'set-cookie',
+    'x-request-id',
+    'x-frame-options',
+    'access-control-allow-origin',
+    'x-xss-protection',
+    'x-content-type-options',
+    'referrer-policy',
+    'content-security-policy',
+    'strict-transport-security',
+    'cf-cache-status'
+];
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.post('/proxyreq', function (req, res, next) {
+app.post('/proxyreq', (req, res, next) =>{
     console.log('proxyreq');
-    // console.log('req body: ', req.body);
-    // console.log('req.body.uri: ', req.body.uri);
     console.log('req.body[\'uri\']: ', req.body['uri']);
     console.log('req.body[\'method\']: ', req.body['method']);
     console.log('req.body[\'body\']: ', req.body['body']);
@@ -24,20 +39,20 @@ app.post('/proxyreq', function (req, res, next) {
     if(bodyReq === undefined) {
         bodyReq = null;
     }
-    request({
-        uri: req.body['uri'],
+    const contenttype = null;
+    if(req.body['json'] === true) {
+        contenttype = 'application/json';
+    }
+    fetch(req.body['uri'], {
         method: req.body['method'],
         body: bodyReq,
-        json: req.body['json'],
-        function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log(body);
-                res.json(body);
-            } else {
-                res.json(error);
-            }
-        }
-    }).pipe(res);
+        headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log(json);
+        res.json(json);
+    });
 });
 
 app.listen(port, () => {
